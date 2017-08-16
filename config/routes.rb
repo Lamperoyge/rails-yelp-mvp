031306931a3f5d39aaca0eb06585a011a7462d93
Adrian Mustea <mustea.adrian95@gmail.com>
2017-08-16 12:48:38 +0100

    works fine

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+root 'restaurants#index'
+
+  resources :restaurants, only:[:index, :show, :create, :new] do
+    resources :reviews, only:[:create, :new]
+    end
+  namespace :admin do
+    resources :restaurants
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "chinese",
+    phone_number: "01 43 54 23 31"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "italian",
+    phone_number: "01 20 20 56 32"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "japanese",
+    phone_number: "01 22 44 66 88"
+  },
+
+  {
+    name:         "McDonalds",
+    address:      "everywhere 14-22",
+    category:     "belgian",
+    phone_number: "01 22 44 66 88"
+  },
+
+
+  {
+    name:         "Nobu",
+    address:      "somewherecool 75016 Paris",
+    category:     "french",
+    phone_number: "01 33 55 77 99"
+  }
+]
+
+
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
